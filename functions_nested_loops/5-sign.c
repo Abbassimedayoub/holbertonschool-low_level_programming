b1545ec88513d37967ab867c6494f9b7cba1d37f
abbassimedayoub <abbassimedyoub@gmail.com>
2023-10-17 05:46:21 -0700

    check the sign of number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - function that prints the sing of number
+ * @n: the number to be cheket
+ *
+ * Return: 1 if it is positive -1 if negative 0 if it zero
+ */
+int print_sign(int n)
+{
+if (n < 0)
+	return (-1);
+else if (n > 0)
+	return (1);
+else
+	return (0);
+}
