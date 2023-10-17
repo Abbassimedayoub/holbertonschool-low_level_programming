6e755db1eab3369c285e5af5f0353f129aec8156
abbassimedayoub <abbassimedyoub@gmail.com>
2023-10-17 07:20:16 -0700

    diplay the pytagore table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,20 +12,23 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
+			m = i * j;
 			if (j != 0)
+			{	
 			_putchar(',');
 			_putchar(' ');
-			m = i * j;
-			else if (m < 10)
+			}
+			if (m < 10)
 			{
+			_putchar(' ');	
                         _putchar(m + '0');
-                        _putchar(' ');
-                        }}
+                        }
 			else
 			{
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			}
+		}
 		_putchar('\n');
 	}
 }
