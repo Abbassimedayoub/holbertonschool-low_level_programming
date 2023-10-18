ae30bdb62b27658600ed33e0b9bbac30f2998daa
abbassimedayoub <abbassimedyoub@gmail.com>
2023-10-18 12:59:16 -0700

    task

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int num = n; // to convert n into his absulte value the manipulate it easly
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10); // rpeat the function till num < 0
+
+	_putchar((num % 10) + '0');
+}
