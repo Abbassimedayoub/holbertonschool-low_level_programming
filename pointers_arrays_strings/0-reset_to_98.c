331a94cb4c6dace999e5da2d2954004beed0fdbb
abbassimedayoub <abbassimedyoub@gmail.com>
2023-10-23 01:04:08 -0700

    reset the value of entry n to spesific number in our case is 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - function that return all int value to 98
- * @*n: the entring number
+ * @n: the entring number
  * Return: an int that must have the value of 98
  */
 void reset_to_98(int *n)
