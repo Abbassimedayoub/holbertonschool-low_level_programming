294632c0decc84103792bdee6f5e1d05f673f6c2
abbassimedayoub <abbassimedyoub@gmail.com>
2023-10-23 01:08:27 -0700

    function that switch tow integer

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * swap_int - function that switch tow number
+ * @a: the first int
+ * @b: the second number
+ * Return: nothing to return void
+ */
+void swap_int(int *a, int *b)
+{
+	int aux;
+	
+	*b = aux;
+	*b = *a;
+	*a = aux;
+
+}
