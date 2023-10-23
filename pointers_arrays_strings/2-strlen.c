c3568fc4403eba910b1d609db3a41cd3ee93663e
abbassimedayoub <abbassimedyoub@gmail.com>
2023-10-23 01:23:59 -0700

    display the lenght of function

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strlen - function that count the lenght of a string
+ * _strlen - function that count the lenght of a string
  * @s: the string the string to be cheked
  * Return: the lenght of the string
  */
@@ -8,7 +8,7 @@ int _strlen(char *s)
 {
 	int i = 0;
 
-	while(*s != '\0')
+	while( *s != '\0')
 	{
 		s++;
 		i++;
