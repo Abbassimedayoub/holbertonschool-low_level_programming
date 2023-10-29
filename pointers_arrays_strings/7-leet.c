e42f0c8aed7ecaadd12217e7c322ad9f8be3862c
abbassimedayoub <abbassimedyoub@gmail.com>
2023-10-29 02:44:58 -0700

    fix errors

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
   * leet - Encodes a string into 1337
   * @s: The string to encode
@@ -11,8 +11,8 @@ char *leet(char *s)
 	int i, j;
 	char letre[5] = {'A', 'E', 'O', 'T', 'L'};
 	char chifre[5] = {'4', '3', '0', '7', '1'};
-
-	for (i = 0; s[i] != '\0'; i++)
+	
+	for (i = 0; i <= sizeof(*s); i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
