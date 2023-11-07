73efbab77ed13109bfe581f927884fcedacf8674
abbassimedayoub <abbassimedyoub@gmail.com>
2023-11-07 01:35:40 -0800

    function duplicate with memo allocation

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - function that return a duplucated string
+ * @str: the string to be duplicated
+ * Return: a duplucated srtring
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+
+	ptr = (char *)malloc(strlen(str) + 1);
+	if (ptr == NULL)
+		return (0);
+	strcpy(ptr, str);
+	return (ptr);
+	free(ptr);
+}
