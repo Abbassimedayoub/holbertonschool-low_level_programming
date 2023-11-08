7f7c44d70d5607169c37a23b332ad2de69024bf5
abbassimedayoub <abbassimedyoub@gmail.com>
2023-11-08 03:59:14 -0800

    task

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	ptr = malloc(sizeof(int *) * height);
+	ptr = (int **)malloc(sizeof(int *) * height);
 		if (ptr == NULL)
 			return (NULL);
 	for (i = 0; i < height; i++)
