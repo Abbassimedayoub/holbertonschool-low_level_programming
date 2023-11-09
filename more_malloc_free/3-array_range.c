03e3fd607ad444ffec7c5dcaf9874ad13ace35ba
abbassimedayoub <abbassimedyoub@gmail.com>
2023-11-09 06:38:42 -0800

    task

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = max - min;
-	ar = malloc((size + 1) *sizeof(int));
+	ar = malloc((size + 1) * sizeof(int));
 	if (ar == NULL)
 		return (NULL);
 	i = 0;
