dad0b275e589071b5d6225064d5e3106b444a411
abbassimedayoub <abbassimedyoub@gmail.com>
2023-11-13 02:12:32 -0800

    create a typedef

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(p));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	if (name == NULL)
+	{
+		free(name);
+		free(p);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(owner);
+		free(p);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+}
