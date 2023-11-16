5432ed809b77ca2772f7b484c2fcba334b162e0d
abbassimedayoub <abbassimedyoub@gmail.com>
2023-11-16 00:52:13 -0800

    betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,16 +1,14 @@
 #include "function_pointers.h"
 
 /**
- *print_name - function that print a name
- *@name: parameter of the func
- *@f: pointer to a func
- *
- * Return: void
+ * print_name - Function that print a name
+ *@name: Parameter of the func
+ *@f: Pointer to a func
+ *Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (f != 0)
 		(f)(name);
-
 }
