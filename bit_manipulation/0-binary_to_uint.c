8867bdce89741f427305d3e91ce6e77d926065db
abbassimedayoub <6708@holbertonstudents.com>
2023-12-01 05:27:23 -0800

    fix the betty error

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,31 @@
 #include "main.h"
 /**
-* binary_to_uint - Converts a binary string
-* @b: Pointer to a binary string
-*
-* Return: The converted unsigned integer, or 0 if the string is NULL or
-* contains characters other than 0 or 1.
-*/
+ * binary_to_uint - function that converts binary to unsigned i
+ * @b: pointing to string of 0 1
+ * Return: result
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int output = 0;
-	int i;
+	int i = 0;
+	unsigned int result = 0;
 
 	if (b == NULL)
-	{
 		return (0);
-	}
 
-	for (i = 0; b[i] != '\0'; i++)
+	while (b[i])
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] == '0' || b[i] == '1')
+		{
+
+			result = result << 1;
+			result += (b[i] - '0');
+		}
+		else
 		{
 			return (0);
 		}
-		output = (output << 1) + (b[i] - '0');
+		i++;
 	}
-	return (output);
+	return (result);
 }
--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,28 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* binary_to_uint - Converts a binary string
-* @b: Pointer to a binary string
-*
-* Return: The converted unsigned integer, or 0 if the string is NULL or
-* contains characters other than 0 or 1.
-*/
-unsigned int binary_to_uint(const char *b)
+ * print_binary - function  that returns the value of a bit
+ * @n: unsigned int
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
 {
-	unsigned int output = 0;
-	int i;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (b == NULL)
-	{
-		return (0);
-	}
-
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-		{
-			return (0);
-		}
-		output = (output << 1) + (b[i] - '0');
-	}
-	return (output);
+	if (n & 1)
+		putchar('1');
+	else
+		putchar('0');
 }
